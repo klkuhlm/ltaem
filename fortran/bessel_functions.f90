1045d806d4840b355e1c7f9384c6a72c59e16ae2
klkuhlm <kristopher.kuhlman@gmail.com>
2010-05-27 14:35:50 -0600

    starting to implement flux effects on other elements, realized I need to switch orders of arguments in bessel function wrapping routines (again...)

--- fortran/bessel_functions.f90
+++ fortran/bessel_functions.f90
@@ -30,13 +30,13 @@ contains
   function besk_matz(z,num) result(besk)
     complex(DP), dimension(:,:), intent(in) :: z
     integer, intent(in) :: num
-    complex(DP), dimension(0:num-1,size(z,1),size(z,2)) :: besk
+    complex(DP), dimension(size(z,1),size(z,2),0:num-1) :: besk
     integer :: nz, ierr, i,j
     integer, parameter :: kode = 1
 
     do i = 1, size(z,1)
        do j = 1, size(z,2)
-          call cbesk(z(i,j), 0.0_DP, kode, num, besk(0:num-1,i,j), nz, ierr)
+          call cbesk(z(i,j), 0.0_DP, kode, num, besk(i,j,0:num-1), nz, ierr)
           ! either 0 or 3 are acceptable return codes
           if ((ierr >= 1 .and. ierr <= 2) .or. ierr >= 4) then
              write(*,'(A,4(1X,I0))') 'besk_matz error',nz,ierr,i,j
@@ -50,8 +50,8 @@ contains
   function besk_vectz(z,num) result(besk)
     complex(DP), dimension(:), intent(in) :: z
     integer, intent(in) :: num
-    complex(DP), dimension(0:num-1,size(z,1)) :: besk
-    besk = sum(besk_matz(spread(z,dim=2,ncopies=1),num),dim=3)
+    complex(DP), dimension(size(z,1),0:num-1) :: besk
+    besk = sum(besk_matz(spread(z,dim=2,ncopies=1),num),dim=2)
   end function besk_vectz
 
   ! K Bessel function for scalar argument / vector of N
@@ -59,20 +59,20 @@ contains
     complex(DP), intent(in) :: z
     integer, intent(in) :: num
     complex(DP), dimension(0:num-1) :: besk
-    besk = sum(sum(besk_matz(spread([z],dim=2,ncopies=1),num),dim=3),dim=2)
+    besk = sum(sum(besk_matz(spread([z],dim=2,ncopies=1),num),dim=2),dim=1)
   end function besk_zscal
 
   ! I Bessel function for 2D matrix argument / vector of N
   function besi_matz(z,num) result(besi)
     complex(DP), dimension(:,:), intent(in) :: z
     integer, intent(in) :: num
-    complex(DP), dimension(0:num-1,size(z,1),size(z,2)) :: besi
+    complex(DP), dimension(size(z,1),size(z,2),0:num-1) :: besi
     integer :: nz, ierr, i,j
     integer, parameter :: kode = 1
 
     do i = 1, size(z,1)
        do j = 1, size(z,2)
-          call cbesi(z(i,j), 0.0_DP, kode, num, besi(0:num-1,i,j), nz, ierr)
+          call cbesi(z(i,j), 0.0_DP, kode, num, besi(i,j,0:num-1), nz, ierr)
           ! either 0 or 3 are acceptable return codes
           if ((ierr >= 1 .and. ierr <= 2) .or. ierr >= 4) then
              write(*,'(A,4(1X,I0))') 'besi_matz error',nz,ierr,i,j
@@ -86,8 +86,8 @@ contains
   function besi_vectz(z,num) result(besi)
     complex(DP), dimension(:), intent(in) :: z
     integer, intent(in) :: num
-    complex(DP), dimension(0:num-1,size(z,1)) :: besi
-    besi = sum(besi_matz(spread(z,dim=2,ncopies=1),num),dim=3)
+    complex(DP), dimension(size(z,1),0:num-1) :: besi
+    besi = sum(besi_matz(spread(z,dim=2,ncopies=1),num),dim=2)
   end function besi_vectz
 
   ! I Bessel function for scalar argument / vector of N
@@ -95,7 +95,7 @@ contains
     complex(DP), intent(in) :: z
     integer, intent(in) :: num
     complex(DP), dimension(0:num-1) :: besi
-    besi = sum(sum(besi_matz(spread([z],dim=2,ncopies=1),num),dim=3),dim=2)
+    besi = sum(sum(besi_matz(spread([z],dim=2,ncopies=1),num),dim=2),dim=1)
   end function besi_zscal
 
   ! use recurrance relationships for derivatives
@@ -105,22 +105,21 @@ contains
   subroutine besId_zmat(z,n,I,ID)
     complex(DP), dimension(:,:), intent(in) :: z
     integer, intent(in) :: n
-    complex(DP), intent(out), dimension(0:n-1,size(z,1),size(z,2)) :: I, ID
-
-    I(0:n-1,:,:) = bI(z,n)
-    ID(1:n-2,:,:) = 0.5_DP*(I(0:n-3,:,:) + I(2:n-1,:,:))      ! middle
-    ID(0,:,:) = I(1,:,:)                                      ! low end
-    ID(n-1,:,:) = I(n-2,:,:) - (n-1)/z*I(n-1,:,:) ! high end
+    complex(DP), intent(out), dimension(size(z,1),size(z,2),0:n-1) :: I, ID
+    I(:,:,0:n-1) = bI(z,n)
+    ID(:,:,1:n-2) = 0.5_DP*(I(:,:,0:n-3) + I(:,:,2:n-1))      ! middle
+    ID(:,:,0) = I(:,:,1)                                      ! low end
+    ID(:,:,n-1) = I(:,:,n-2) - (n-1)/z*I(:,:,n-1) ! high end
   end subroutine besId_zmat
 
   subroutine besId_zvect(z,n,I,ID)
     complex(DP), dimension(:), intent(in) :: z
     integer, intent(in) :: n
-    complex(DP), intent(out), dimension(0:n-1,size(z,1)) :: I, ID
-    complex(DP), dimension(0:n-1,size(z,1),1) :: tI, tId
+    complex(DP), intent(out), dimension(size(z,1),0:n-1) :: I, ID
+    complex(DP), dimension(size(z,1),1,0:n-1) :: tI, tId
     call besId_zmat(spread(z,2,1),n,tI,tId)
-    I = tI(:,:,1)
-    ID = tId(:,:,1)
+    I = tI(:,1,:)
+    ID = tId(:,1,:)
   end subroutine besId_zvect
 
   subroutine besId_zscal(z,n,I,ID)
@@ -129,39 +128,38 @@ contains
     complex(DP), intent(out), dimension(0:n-1) :: I, ID
     complex(DP), dimension(1,1,0:n-1) :: tI, tId
     call besId_zmat(spread([z],2,1),n,tI,tId)
-    I = tI(:,1,1)
-    ID = tId(:,1,1)
+    I = tI(1,1,:)
+    ID = tId(1,1,:)
   end subroutine besId_zscal
 
   subroutine besKd_zmat(z,n,K,KD)
     complex(DP), dimension(:,:), intent(in) :: z
     integer, intent(in) :: n
-    complex(DP), intent(out), dimension(0:n-1,size(z,1),size(z,2)) :: K, KD
-
-    K(0:n-1,:,:) = bK(z,n)
-    KD(1:n-2,:,:) = -0.5_DP*(K(0:n-3,:,:) + K(2:n-1,:,:))        ! middle
-    KD(0,:,:) = - K(1,:,:)                                       ! low end
-    KD(n-1,:,:) = -(K(n-2,:,:) + (n-1)/z*K(n-1,:,:)) ! high end
+    complex(DP), intent(out), dimension(size(z,1),size(z,2),0:n-1) :: K, KD
+    K(:,:,0:n-1) = bK(z,n)
+    KD(:,:,1:n-2) = -0.5_DP*(K(:,:,0:n-3) + K(:,:,2:n-1))        ! middle
+    KD(:,:,0) = - K(:,:,1)                                       ! low end
+    KD(:,:,n-1) = -(K(:,:,n-2) + (n-1)/z*K(:,:,n-1)) ! high end
   end subroutine besKd_zmat
 
   subroutine besKd_zvect(z,n,K,KD)
     complex(DP), dimension(:), intent(in) :: z
     integer, intent(in) :: n
-    complex(DP), intent(out), dimension(0:n-1,size(z,1)) :: K, KD
-    complex(DP), dimension(0:n-1,size(z,1),1) :: tK, tKd
+    complex(DP), intent(out), dimension(size(z,1),0:n-1) :: K, KD
+    complex(DP), dimension(size(z,1),1,0:n-1) :: tK, tKd
     call besKd_zmat(spread(z,2,1),n,tK,tKd)
-    K = tK(:,:,1)
-    KD = tKd(:,:,1)
+    K = tK(:,1,:)
+    KD = tKd(:,1,:)
   end subroutine besKd_zvect
 
   subroutine besKd_zscal(z,n,K,KD)
     complex(DP), intent(in) :: z
     integer, intent(in) :: n
     complex(DP), intent(out), dimension(0:n-1) :: K, KD
-    complex(DP), dimension(0:n-1,1,1) :: tK, tKd
+    complex(DP), dimension(1,1,0:n-1) :: tK, tKd
     call besKd_zmat(spread([z],2,1),n,tK,tKd)
-    K = tK(:,1,1)
-    KD = tKd(:,1,1)
+    K = tK(1,1,:)
+    KD = tKd(1,1,:)
   end subroutine besKd_zscal
 
 end module bessel_functions
--- fortran/elements.f90
+++ fortran/elements.f90
@@ -189,59 +189,59 @@ contains
             & cmat(M,0:N-1), smat(M,1:N-1))
        cmat = cos(outerprod(c%G(targ)%Pgm(1:M), real([(j,j=0,N-1)],DP)))
        smat = sin(outerprod(c%G(targ)%Pgm(1:M), real([(j,j=1,N-1)],DP)))
-    end if
-    
-    ! can the target element "see" the outside of the source element?
-    if (dom%inclBg(src,targ)) then
-       allocate(tmp(M,2*N-1))
 
-       ! setup LHS (head effects due to source element)
-       ! for constant head (-1), or matching (0)
-       if (el%ibnd==0 .or. el%ibnd==-1) then
-          allocate(Kn(M,0:N-1),Kn0(0:N-1))
-          kap = kappa(p,c%parent)
-          Kn(0:N-1,1:M) = transpose(bK(kap*c%G(targ)%Rgm(1:M),N))
-          Kn0(0:N-1) = bK(kap*c%r,N)
+       ! can the target element "see" the outside of the source element?
+       if (dom%inclBg(src,targ)) then
+          allocate(tmp(M,2*N-1))
 
-          ! head effects on other element
-          tmp(1:M,1:N) =       Kn(0:N-1,:)/spread(Kn0(0:N-1),1,M)*cmat/c%parent%K
-          tmp(1:M,N+1:2*N-1) = Kn(1:N-1,:)/spread(Kn0(1:N-1),1,M)*smat/c%parent%K
-          deallocate(Kn,Kn0)
-
-          select case (c%ibnd)
-          case(2)
-             allocate(Kn0(0:1))
+          ! setup LHS (head effects due to source element)
+          ! for constant head (-1), or matching (0)
+          if (el%ibnd==0 .or. el%ibnd==-1) then
+             allocate(Kn(M,0:N-1),Kn0(0:N-1))
              kap = kappa(p,c%parent)
-             Kn0(0:1) = bK(kap*c%r,2)
+             Kn(0:N-1,1:M) = transpose(bK(kap*c%G(targ)%Rgm(1:M),N))
+             Kn0(0:N-1) = bK(kap*c%r,N)
 
-             if (c%StorIn) then
-                ! wellbore storage and skin from finite-radius well
-                r%LHS(1:M,1) = -Kn0(0)*((2.0 + c%r**2*c%dskin*p/c%parent%T)/(2.0*PI*c%r) + &
-                     & (Kn0(0)*c%r*p)/(2.0*PI*c%r*kap*Kn0(1)*c%parent%T))*tmp(1:M,1)
-                r%RHS(1:M) = 0.0
-             else
-                ! specified flux (finite-radius well no storage)
-                R%LHS(1:M,1) = 0.0
-                r%RHS(1:M) = Kn0(0)*time(p,c%time,.false.)*c%bdryQ/(2.0*PI*c%r*Kn0(1))*tmp(1:M,1)
-             end if
-          case(-1,0,1)
-             r%LHS(1:M,1:2*N-1) = tmp(1:M,1:2*N-1)
-          end select
-       end if
-    elseif (dom%InclIn(src,targ)) then
-       ! can target element "see" the inside of the source element?
-       !  i.e., is the source element the parent?
+             ! head effects on other element
+             tmp(1:M,1:N) =       Kn(0:N-1,:)/spread(Kn0(0:N-1),1,M)*cmat/c%parent%K
+             tmp(1:M,N+1:2*N-1) = Kn(1:N-1,:)/spread(Kn0(1:N-1),1,M)*smat/c%parent%K
+             deallocate(Kn,Kn0)
 
-       if (el%ibnd==0 .or. el%ibnd==-1) then
-          allocate(In(M,0:N-1),In0(0:N-1))
-          kap = kappa(p,c%element)
-          In(0:N-1,1:M) = transpose(bI(kap*c%G(targ)%Rgm(1:M),N))
-          In0(0:N-1) = bI(kap*c%r,N)
+             select case (c%ibnd)
+             case(2)
+                allocate(Kn0(0:1))
+                kap = kappa(p,c%parent)
+                Kn0(0:1) = bK(kap*c%r,2)
 
-          ! head effects on other element
-          r%LHS(1:M,1:N) =       In(0:N-1,:)/spread(In0(0:N-1),1,M)*cmat/c%K
-          r%LHS(1:M,N+1:2*N-1) = In(1:N-1,:)/spread(In0(1:N-1),1,M)*smat/c%K
-          deallocate(In,In0)
+                if (c%StorIn) then
+                   ! wellbore storage and skin from finite-radius well
+                   r%LHS(1:M,1) = -Kn0(0)*((2.0 + c%r**2*c%dskin*p/c%parent%T)/(2.0*PI*c%r) + &
+                        & (Kn0(0)*c%r*p)/(2.0*PI*c%r*kap*Kn0(1)*c%parent%T))*tmp(1:M,1)
+                   r%RHS(1:M) = 0.0
+                else
+                   ! specified flux (finite-radius well no storage)
+                   R%LHS(1:M,1) = 0.0
+                   r%RHS(1:M) = Kn0(0)*time(p,c%time,.false.)*c%bdryQ/(2.0*PI*c%r*Kn0(1))*tmp(1:M,1)
+                end if
+             case(-1,0,1)
+                r%LHS(1:M,1:2*N-1) = tmp(1:M,1:2*N-1)
+             end select
+          end if
+       else
+          ! can target element "see" the inside of the source element?
+          !  i.e., is the source element the parent?
+
+          if (el%ibnd==0 .or. el%ibnd==-1) then
+             allocate(In(M,0:N-1),In0(0:N-1))
+             kap = kappa(p,c%element)
+             In(0:N-1,1:M) = transpose(bI(kap*c%G(targ)%Rgm(1:M),N))
+             In0(0:N-1) = bI(kap*c%r,N)
+
+             ! head effects on other element
+             r%LHS(1:M,1:N) =       In(0:N-1,:)/spread(In0(0:N-1),1,M)*cmat/c%K
+             r%LHS(1:M,N+1:2*N-1) = In(1:N-1,:)/spread(In0(1:N-1),1,M)*smat/c%K
+             deallocate(In,In0)
+          end if
        end if
     end if
   end function circle_match_head_other
@@ -257,16 +257,16 @@ contains
     type(matching), intent(in) :: el ! target element (circle or ellipse)
     type(domain), intent(in) :: dom
     complex(DP), intent(in) :: p
-    type(match_result) :: r 
+    type(match_result) :: r
 
     integer :: j, src, targ, N, M
     real(DP), allocatable :: cmat(:,:), smat(:,:)
     complex(DP), allocatable :: Kn(:,:), Kn0(:), In(:,:), In0(:)
-    complex(DP), allocatable :: tmp(:,:)
+    complex(DP), allocatable :: tmp(:,:,:)
     complex(DP) :: kap
 
     N = c%N ! number of coefficients in the source circular element
-    targ = el%id 
+    targ = el%id
     src = c%id
 
     if (dom%inclBg(src,targ) .or. dom%InclIn(src,targ)) then
@@ -276,63 +276,77 @@ contains
             & cmat(M,0:N-1), smat(M,1:N-1))
        cmat = cos(outerprod(c%G(targ)%Pgm(1:M), real([(j,j=0,N-1)],DP)))
        smat = sin(outerprod(c%G(targ)%Pgm(1:M), real([(j,j=1,N-1)],DP)))
-    end if
-    
-    ! can the target element "see" the outside of the source element?
-    if (dom%inclBg(src,targ)) then
-       allocate(tmp(M,2*N-1))
 
-       ! setup LHS (flux effects due to source element)
-       ! for constant head (-1), or matching (0)
-       if (el%ibnd==0 .or. el%ibnd==-1) then
-          allocate(Kn(M,0:N-1),Kn0(0:N-1))
-          kap = kappa(p,c%parent)
-          Kn(0:N-1,1:M) = transpose(bK(kap*c%G(targ)%Rgm(1:M),N))
-          Kn0(0:N-1) = bK(kap*c%r,N)
+       ! can the target element "see" the outside of the source element?
+       if (dom%inclBg(src,targ)) then
+          allocate(tmp(M,2*N-1),2)
 
-          ! head effects on other element
-          tmp(1:M,1:N) =       Kn(0:N-1,:)/spread(Kn0(0:N-1),1,M)*cmat/c%parent%K
-          tmp(1:M,N+1:2*N-1) = Kn(1:N-1,:)/spread(Kn0(1:N-1),1,M)*smat/c%parent%K
-          deallocate(Kn,Kn0)
+          ! setup LHS (flux effects due to source element)
+          ! for constant head (-1), or matching (0)
+          if (el%ibnd==0 .or. el%ibnd==-1) then
 
-          select case (c%ibnd)
-          case(2)
-             allocate(Kn0(0:1))
-             kap = kappa(p,c%parent)
-             Kn0(0:1) = bK(kap*c%r,2)
+             ! flux effects of source circle on target element
+             ! part 1: derivative wrt radius of source element
+             allocate(Kn(M,0:N),dKn(M,0:N),Kn0(0:N-1))
+             kap = kappa(p,c%parent) 
+             call bKD(kap*c%G(targ)%Rgm(1:M),N+1,Kn,dKn)
+             Kn0(0:N-1) = bK(kap*c%r,N)
+             dKn = kap*dKn
+             
+             tmp(1:M,1:N,1) =       spread(dKn(0:N-1)/Kn(0:N-1), 1,M)*cmat/c%parent%K
+             tmp(1:M,N+1:2*N-1,1) = spread(dKn(1:N-1)/Kn(1:N-1), 1,M)*smat/c%parent%K
+             deallocate(dKn)
 
-             if (c%StorIn) then
-                ! wellbore storage and skin from finite-radius well
-                r%LHS(1:M,1) = -Kn0(0)*((2.0 + c%r**2*c%dskin*p/c%parent%T)/(2.0*PI*c%r) + &
-                     & (Kn0(0)*c%r*p)/(2.0*PI*c%r*kap*Kn0(1)*c%parent%T))*tmp(1:M,1)
-                r%RHS(1:M) = 0.0
-             else
-                ! specified flux (finite-radius well no storage)
-                R%LHS(1:M,1) = 0.0
-                r%RHS(1:M) = Kn0(0)*time(p,c%time,.false.)*c%bdryQ/(2.0*PI*c%r*Kn0(1))*tmp(1:M,1)
-             end if
-          case(-1,0,1)
-             r%LHS(1:M,1:2*N-1) = tmp(1:M,1:2*N-1)
-          end select
+             ! part 2: derivative wrt angle of source element
+             tmp(1:M,1:N,2) =       Kn(0:N-1,:)/spread(Kn0(0:N-1),1,M)*cmat/c%parent%K
+             tmp(1:M,N+1:2*N-1,2) = Kn(1:N-1,:)/spread(Kn0(1:N-1),1,M)*smat/c%parent%K
+
+             ! part 3: project these from cylindrical onto Cartesian coordinates
+
+
+             ! part 4: project from Cartesian to "radial" coordinate of target element
+
+             deallocate(Kn,Kn0)
+
+             select case (c%ibnd)
+             case(2)
+                allocate(Kn0(0:1))
+                kap = kappa(p,c%parent)
+                Kn0(0:1) = bK(kap*c%r,2)
+
+                if (c%StorIn) then
+                   ! wellbore storage and skin from finite-radius well
+                   r%LHS(1:M,1) = -Kn0(0)*((2.0 + c%r**2*c%dskin*p/c%parent%T)/(2.0*PI*c%r) + &
+                        & (Kn0(0)*c%r*p)/(2.0*PI*c%r*kap*Kn0(1)*c%parent%T))*tmp(1:M,1)
+                   r%RHS(1:M) = 0.0
+                else
+                   ! specified flux (finite-radius well no storage)
+                   R%LHS(1:M,1) = 0.0
+                   r%RHS(1:M) = Kn0(0)*time(p,c%time,.false.)*c%bdryQ/(2.0*PI*c%r*Kn0(1))*tmp(1:M,1)
+                end if
+             case(-1,0,1)
+                r%LHS(1:M,1:2*N-1) = tmp(1:M,1:2*N-1)
+             end select
+          end if
+       else
+          ! can target element "see" the inside of the source element?
+          !  i.e., is the source element the parent?
+
+          if (el%ibnd==0 .or. el%ibnd==-1) then
+             allocate(In(M,0:N-1),In0(0:N-1))
+             kap = kappa(p,c%element)
+             In(0:N-1,1:M) = transpose(bI(kap*c%G(targ)%Rgm(1:M),N))
+             In0(0:N-1) = bI(kap*c%r,N)
+
+             ! head effects on other element
+             r%LHS(1:M,1:N) =       In(0:N-1,:)/spread(In0(0:N-1),1,M)*cmat/c%K
+             r%LHS(1:M,N+1:2*N-1) = In(1:N-1,:)/spread(In0(1:N-1),1,M)*smat/c%K
+             deallocate(In,In0)
+          end if
        end if
-    elseif (dom%InclIn(src,targ)) then
-       ! can target element "see" the inside of the source element?
-       !  i.e., is the source element the parent?
-
-       if (el%ibnd==0 .or. el%ibnd==-1) then
-          allocate(In(M,0:N-1),In0(0:N-1))
-          kap = kappa(p,c%element)
-          In(0:N-1,1:M) = transpose(bI(kap*c%G(targ)%Rgm(1:M),N))
-          In0(0:N-1) = bI(kap*c%r,N)
-
-          ! head effects on other element
-          r%LHS(1:M,1:N) =       In(0:N-1,:)/spread(In0(0:N-1),1,M)*cmat/c%K
-          r%LHS(1:M,N+1:2*N-1) = In(1:N-1,:)/spread(In0(1:N-1),1,M)*smat/c%K
-          deallocate(In,In0)
-       end if
     end if
+
   end function circle_match_flux_other
-
 
   ! ##################################################
   ! general time behavior function, for all elements
