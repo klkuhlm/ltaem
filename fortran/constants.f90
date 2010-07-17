e226b0076e7f89ee6fce923fadf5828f5de692c0
klkuhlm <kristopher.kuhlman@gmail.com>
2010-07-17 13:06:43 -0600

    more dinking around with constants

--- fortran/constants.f90
+++ fortran/constants.f90
@@ -4,21 +4,21 @@ module constants
   public
   
   ! real with range 300 orders of mag, 15 sig figs (8 on both g95 & ifort)
-  integer, parameter :: DP = selected_real_kind (p=15,r=300)
+  integer, parameter :: DP = selected_real_kind(p=15,r=300)
 
   ! length of filenames 
   integer, parameter :: lenFN = 128
 
   ! useful? constants related to pi and ln
-  real(kind=DP), parameter :: PI =      4.0_DP*atan(1.0_DP) ! 3.14159...
-  real(kind=DP), parameter :: PIOV2 =   2.0_DP*atan(1.0_DP) ! 1.57079...
+  real(kind=DP), parameter :: PI = 4.0_DP*atan(1.0_DP) ! 3.1415926535897931...
+  real(kind=DP), parameter :: PIOV2 = 2.0_DP*atan(1.0_DP) ! 1.5707963267948966...
 
   ! these are both used in YNOT approximation to elliptical circumference
-  real(kind=DP), parameter :: LN2 =     log(2.0_DP) ! 0.693147...
-  real(kind=DP), parameter :: LNPIOV2 = log(PIOV2)  ! 0.451582...
+  real(kind=DP), parameter :: LN2 = log(2.0_DP) ! 0.69314718055994529...
+  real(kind=DP), parameter :: LNPIOV2 = log(2.0_DP*atan(1.0_DP))  ! 0.45158270528945482...
 
   ! sqrt(-1)
-  complex(kind=DP), parameter :: EYE = cmplx(0.0,1.0,DP)
+  complex(kind=DP), parameter :: EYE = cmplx(0.0_DP,1.0_DP,DP)
 
 end module constants
 
