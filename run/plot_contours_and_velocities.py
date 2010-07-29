5a1d8e69cbef7f610b8a1feba368bc1c5704923b
klkuhlm <kristopher.kuhlman@gmail.com>
2010-07-28 23:29:26 -0600

    creating particle tracking figure, hydrograph figure, and other tweaks to examples

--- run/plot_contours_and_velocities.py
+++ run/plot_contours_and_velocities.py
@@ -8,6 +8,8 @@ else:
     print 'pass base filename as argument'
     exit
 
+particle = False
+
 t = np.atleast_1d(np.loadtxt(basefn+'_t.dat'))
 nt = t.shape[0]
 X = np.loadtxt(basefn+'_x.dat')
@@ -36,27 +38,49 @@ for row in fh:
     else:
         elements[-1].append([float(x) for x in row.split()])
 
+fh.close()
 del elements[-1] # EOF comment at end
+
+if particle:
+    fh = open(basefn.replace('_contour.xyz','_particles.dat'),'r')
+    particles = []
+    junk = fh.readline()
+    for row in fh:
+        if row[0] == '#':
+            particles.append([])
+        else:
+            particles[-1].append([float(x) for x in row.split()])
+    fh.close()
+    del particles[-1]
 
 for val in range(nt):
     plt.figure(1)
     plt.subplot(121)
-    plt.contour(X,Y,h[:,:,val],50)
+    plt.contour(X,Y,h[:,:,val],10)
     plt.axis('image')
     plt.xlabel('X')
     plt.ylabel('Y')
+    plt.plot([0.0,-0.2],[-0.1,0.1],'k*') 
     for el in elements:
         e = np.array(el[:][:-2])
-        plt.plot(e[:,0],e[:,1],'r-',lw=3)
+        plt.plot(e[:,0],e[:,1],'r-',lw=1)
+    if particle:
+        print 'len',len(particles)
+        for part in particles:
+            p = np.array(part[:][:-2])
+            plt.plot(p[:,1],p[:,2],'k-',lw=0.5)
     
     plt.subplot(122)
     plt.contourf(X,Y,np.log10(np.abs(v[:,:,0,val]+v[:,:,1,val]*1j)),20)
-    plt.quiver(X[::4,::4],Y[::4,::4],v[::4,::4,0,val],v[::4,::4,1,val])
+##    plt.contour(X,Y,h[:,:,val],levels=[0])
+    plt.quiver(X[::4,::4],Y[::4,::4],v[::4,::4,0,val],v[::4,::4,1,val],scale=1000)
+
+    
     plt.axis('image')
     plt.xlabel('X')
     for el in elements:
         e = np.array(el[:][:-2])
-        plt.plot(e[:,0],e[:,1],'r-',lw=3)
+        plt.plot(e[:,0],e[:,1],'r-',lw=1)
     
     plt.savefig('%s_%4.4i_.eps' %(basefn,val))
     plt.close(1)
