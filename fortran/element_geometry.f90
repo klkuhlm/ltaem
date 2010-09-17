483e7d8f5327e6088ac7dc8e76113d5fbbbb3b83
kris <devnull@localhost>
2010-09-16 22:15:44 -0600

    more progress on heirarchy calcs.  getting there.

--- fortran/element_geometry.f90
+++ fortran/element_geometry.f90
@@ -251,9 +251,10 @@ contains
     type(ellipse), dimension(:), intent(in) :: e
     integer :: nc, ne, ntot
 
-    real(DP), allocatable :: Rcg(:,:), Eeg(:,:)
+    real(DP), allocatable :: Rcg(:,:), Eeg(:,:), R(:)
     complex(DP), allocatable :: Z(:,:)
     logical, allocatable :: nondiag(:,:), upper(:,:)
+    integer, allocatable :: iv(:), nest(:)
     integer :: i, j
 
     nc = dom%num(1)
@@ -282,16 +283,22 @@ contains
        ! check centers of elements (rows = circles, columns = all elements)
        if (nc > 0) then
           allocate(Rcg(nc,ntot))
-          Rcg(1:nc,1:nc) =      abs(spread(cmplx(c%x,c%y,DP),1,nc) - spread(cmplx(c%x,c%y,DP),2,nc))
-          Rcg(1:nc,nc+1:ntot) = abs(spread(cmplx(e%x,e%y,DP),1,nc) - spread(cmplx(c%x,c%y,DP),2,nc))
+          Rcg(1:nc,1:nc) =      abs(spread(cmplx(c%x,c%y,DP),1,nc) - &
+                                  & spread(cmplx(c%x,c%y,DP),2,nc))
+          Rcg(1:nc,nc+1:ntot) = abs(spread(cmplx(e%x,e%y,DP),1,nc) - &
+                                  & spread(cmplx(c%x,c%y,DP),2,ne))
           
           ! nondiag handles zero distance-to-self case
-          where (Rcg(1:nc,1:ntot) < spread(c%r,2,ntot) .and. nondiag)
+          where (Rcg(1:nc,1:ntot) < spread(c(1:nc)%r,2,ntot) .and. nondiag)
+             ! Is center of target element (2nd dim)
+             ! inside radius of source element (1st dim)?
              dom%InclIn(1:nc,1:ntot) = .true.
           end where
 
-          deallocate(Rcg)
-          
+          ! two cases where one element center is inside another
+          !  1) elements intersect (2 pts) or touch (1 point) = BAD
+          !  2) smaller element inside larger element         = OK
+
           ! check circle-on-circle intersection
           do i = 1, nc
              do j = 1, nc
@@ -318,7 +325,6 @@ contains
                 end if
              end do
           end do
-          deallocate(Rcg)
        end if
        
        ! ## step 2 ####################
@@ -327,17 +333,20 @@ contains
        ! check centers of elements (rows = ellipses, columns = all elements)
        if (ne > 0) then
           allocate(Eeg(ne,ntot),Z(ne,ntot))
-          Z(1:ne,1:nc) = spread(cmplx(c%x,c%y,DP),1,ne) - spread(cmplx(e%x,e%y,DP),2,nc)
-          Eeg(1:ne,1:nc) = real(cacosh(Z(1:ne,1:nc)*spread(exp(-EYE*e%theta)/e%f,2,nc)))
+          Z(1:ne,1:nc) =      spread(cmplx(c%x,c%y,DP),1,ne) - &
+                            & spread(cmplx(e%x,e%y,DP),2,nc)
+          Eeg(1:ne,1:nc) =      real(cacosh(Z(1:ne,1:nc)* &
+                                  & spread(exp(-EYE*e%theta)/e%f,2,nc)))
 
-          Z(1:ne,nc+1:ntot) = spread(cmplx(e%x,e%y,DP),1,ne) - spread(cmplx(e%x,e%y,DP),2,ne)
-          Eeg(1:ne,nc+1:ntot) = real(cacosh(Z(1:ne,nc+1:ntot)*spread(exp(-EYE*e%theta)/e%f,2,ne)))
+          Z(1:ne,nc+1:ntot) = spread(cmplx(e%x,e%y,DP),1,ne) - &
+                            & spread(cmplx(e%x,e%y,DP),2,ne)
+          Eeg(1:ne,nc+1:ntot) = real(cacosh(Z(1:ne,nc+1:ntot)* &
+                                  & spread(exp(-EYE*e%theta)/e%f,2,ne)))
           deallocate(Z)
 
           where (Eeg(1:ne,1:ntot) < spread(e%r,2,ntot) .and. nondiag)
              dom%InclIn(nc+1:ntot,1:ntot) = .true.
           end where
-          deallocate(Eeg)
 
           ! check ellipse-on-circle intersection
           do i = 1, ne
@@ -364,19 +373,46 @@ contains
                 end if
              end do
           end do
-
        end if
+              
+       ! #1# remove bigger elements from inside 
+       ! smaller elements, for the case they are concentric
+       allocate(R(1:ntot), nest(1:ntot))
+       R(1:nc) = c(1:nc)%r
+       R(nc+1:ntot) = e(1:ne)%r  ! TODO : double-check that circ/ellip can be compared validly  
 
-       ! if an element is not inside any other elements, it must be in the background
-       where (.not. any(dom%InclIn(1:ntot,1:ntot),dim=1))
+       forall (i=1:ntot, j=1:ntot, i/=j, &
+            & dom%InclIn(i,j).and.dom%InclIn(j,i), R(i)>R(j))
+          dom%InclIn(i,j) = .false.
+       end forall
+
+       ! number of True values in each column 
+       nest(1:ntot) = count(dom%InclIn(1:ntot,1:ntot),dim=1)  
+
+       ! #2# if an element is not inside any other elements
+       !  (whole column is false) it must be in the background
+       where (all(.not. dom%InclIn(1:ntot,:),dim=1))
+          ! zero row indicating element is inside element zero
           dom%InclIn(0,1:ntot) = .true.
+          dom%InclUp(1:ntot) = 0  
        end where
 
-       ! ## step 4 ####################
-       ! handle multiply-nested cases
+       ! #3# if an element is only listed as being inside one element
+       ! (one element in column is true), it is simple to determine parent
+       iv = [(i,i=1,ntot)]
+       do i=1,ntot
+          if (count(dom%InclIn(1:ntot,i)) == 1) then
+             ! only one true in that column; use it to mask sum of an integer vector
+             dom%InclUp(i) = sum(iv,mask=dom%InclIn(1:ntot,i))
+          end if
+       end do
+       
+       ! #4# handle wells listed as being inside multiple elements
+       do i=1,ntot
+          nest = 
+          if ( > 1) then
+             nest = 
 
-       ! ## step 5 ###################
-       ! determine 'up' and 'bg' relationships from 'in'
 
     else
        ! special case of only one element, no matching -- it is in background
