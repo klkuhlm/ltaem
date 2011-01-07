4797f06b3bbb81278a75ed5d86dc80726c047797
klkuhlm <kristopher.kuhlman@gmail.com>
2011-01-07 13:05:04 -0700

    based on some simple testing, it appears the recurrence is suceptable to cancellation, and only a tiny bit faster.

--- fortran/test-cos-spread.f90
+++ fortran/test-cos-spread.f90
@@ -1,51 +1,50 @@
 program test
   implicit none
 
-  integer, parameter :: DP = 8, N = 25
+  integer, parameter :: DP = 8, N = 200, M=N*10
   integer, dimension(N) :: vi
-  real(DP), dimension(N,N) :: c
-  real(DP), dimension(N) :: x,s
-  complex(DP), dimension(N) :: z
+  real(DP), dimension(M,N) :: c
+  real(DP), dimension(M) :: x !!,s
+!!$  complex(DP), dimension(M) :: z
   integer :: i
+  real(DP) :: t0,t1
   
-  forall (i=0:N-1) vi(i+1) = i
-  forall (i=0:N-1) x(i+1) = 0.2*i
+  intrinsic :: cpu_time
 
-  ! outer product form	(simple, but no recurrence)
-  c = cos(spread(x,2,N)*spread(vi,1,N))
+!!$  forall (i=0:N-1) vi(i+1) = i
+  forall (i=0:M-1) x(i+1) = 0.05*i
 
-  print *, 'matrix ops'
-  write(*,'(3X,12(I21,1X))') vi(1:12)
-  do i=1,N
-     write(*,'(I2,1X,12(F21.18,1X))') i,c(i,1:12)
-  end do
+  call cpu_time(t0)
+
+!!$
+!!$  ! outer product form	(simple, but no recurrence)
+!!$  c = cos(spread(x,2,N)*spread(vi,1,M))
+!!$
   
-  ! de Moivre's recurrence (all orders computed from first one)
-  ! (requires both sine & cosine, uses complex powers)
-  ! http://www.trans4mind.com/personal_development/mathematics/trigonometry/deMoivre.htm
-  c(:,1) = 1.0
-  c(:,2) = cos(x)
-  s(:) = sin(x)
-  z = cmplx(c(:,2),s,DP)
-  forall (i=2:N-1) c(:,i+1) = real(z**i)
-  
-  print *, "de Moivre's recurrance"
-  write(*,'(3X,12(I21,1X))') vi(1:12)
-  do i=1,N
-     write(*,'(I2,1X,12(F21.18,1X))') i,c(i,1:12)
-  end do
-  
+!!$  ! de Moivre's recurrence (all orders computed from first one)
+!!$  ! (requires both sine & cosine, uses complex powers)
+!!$  ! http://www.trans4mind.com/personal_development/mathematics/trigonometry/deMoivre.htm
+!!$  c(:,1) = 1.0
+!!$  c(:,2) = cos(x)
+!!$  s(:) = sin(x)
+!!$  z = cmplx(c(:,2),s,DP)
+!!$  forall (i=2:N-1) c(:,i+1) = real(z**i)
+!!$  
+
   ! Chebyshev 2-level recurrence  (no powers, but must be done in order)
   c = cos_recurrence(x,N)
 
-  print *, "Chebyshev's recurrance"
-  write(*,'(3X,12(I21,1X))') vi(1:12)
-  do i=1,N
-     write(*,'(I2,1X,12(F21.18,1X))') i,c(i,1:12)
-  end do
-  
-  contains
+  call cpu_time(t1)
 
+  print *, 'time:',t1-t0
+
+  write(*,'(3X,200(I23,1X))') vi(1:N)
+  do i=1,M
+     write(*,'(I0,1X,200(F23.20,1X))') i,c(i,1:N)
+  end do
+ 
+
+  contains
   function cos_recurrence(x,n) result(c)
     real(DP), intent(in), dimension(:) :: x
     integer, intent(in) :: n
