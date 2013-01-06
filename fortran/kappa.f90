5bbde1153d766f6cc83ab39dd18787c2f2f4c462
Kris Kuhlman <kristopher.kuhlman@gmail.com>
2013-01-06 16:41:56 -0700

    cleaned up kappa routine, with definition of leaky/unconfined/dual porosity

--- fortran/kappa.f90
+++ fortran/kappa.f90
@@ -44,50 +44,52 @@ contains
     complex(DP), allocatable ::  kap2(:), exp2z(:)
 
     np = size(p)
+
+    !! standard confined definition
+    q(1:np) = p(:)/el%alpha
+
+    !! leaky
+    !! ##############################
     if (el%leakFlag /= 0) then
        allocate(kap2(np),exp2z(np))
        kap2(1:np) = sqrt(p(:)*el%aquitardSs/el%aquitardK)
        exp2z(1:np) = exp(-2.0*kap2(:)*el%aquitardb)
+    
+       select case(el%leakFlag)
+       case(1)
+          !! case I, no-drawdown condition at top of aquitard
+          q(1:np) = q + kap2(:)*el%aquitardK/(el%b*el%K)*&
+               & (1.0 + exp2z(:))/(1.0 - exp2z(:))
+       case(2)
+          !! case II, no-flow condition at top of aquitard
+          q(1:np) = q + kap2(:)*el%aquitardK/(el%b*el%K)*&
+               & (1.0 - exp2z(:))/(1.0 + exp2z(:))
+       case(3)
+          !! aquitard thickness -> infinity
+          q(1:np) = q + kap2(:)*el%aquitardK/(el%b*el%K)
+       end select
+
+       deallocate(kap2,exp2z)
     end if
 
-    !! leakiness
-    !! ##############################
-    select case(el%leakFlag)
-    case(0)
-       !! no leaky layer, standard definition
-       q(1:np) = p(:)/el%alpha
-    case(1)
-       !! case I, no-drawdown condition at top of aquitard
-       q(1:np) = p(:)/el%alpha + kap2(:)*el%aquitardK/(el%b*el%K)*&
-            & (1.0 + exp2z(:))/(1.0 - exp2z(:))
-    case(2)
-       !! case II, no-flow condition at top of aquitard
-       q(1:np) = p(:)/el%alpha + kap2(:)*el%aquitardK/(el%b*el%K)*&
-            & (1.0 - exp2z(:))/(1.0 + exp2z(:))
-    case(3)
-       !! aquitard thickness -> infinity
-       q(1:np) = p(:)/el%alpha + kap2(:)*el%aquitardK/(el%b*el%K)
-    end select
-
-    !! unconfined-ness (if confined do nothing)
+    !! unconfined
     !! ##############################
     if(el%unconfinedFlag) then
-       ! results of integrating Neuman 1972 unconfined solution from 0 -> b in z
-       q(1:np) = q(:) + el%Kz*p(:)/(el%K*el%b*(el%Kz/el%Sy + p(:)))
+       ! integrated (0->b dz) Neuman (1972, WRR) solution 
+       q(1:np) = q + el%Kz*p(:)/(el%K*el%b*(el%Kz/el%Sy + p(:)))
     end if
 
-    !! dual porosity (if single porosity do nothing)
+    !! dual porosity 
     !! ##############################
     if(el%dualPorosityFlag) then
        ! solution adapted from Dougherty and Babu (1984, WRR)
-       q(1:np) = q(:) + &
+       q(1:np) = q + &
             & (1.0 - el%lambda/(el%matrixSs*p(:) + el%lambda))*el%lambda/el%K
     end if
 
-    !! sources are only additive under the square root
+    !! sources additive under square root
     q = sqrt(q)
 
-    if (el%leakFlag /= 0) deallocate(kap2,exp2z)
 
   end function kappa_pVect
 
@@ -99,7 +101,6 @@ contains
     type(element), intent(in) :: el
     complex(DP) :: q
 
-    !! sum away singleton first dimension
     q = sum(kappa_pVect([p],el))
 
   end function kappa_pscal
