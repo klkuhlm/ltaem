28129fb7518c4f11edcb5efd4a22395704cdbc4e
klkuhlm <kristopher.kuhlman@gmail.com>
2013-09-23 13:40:14 -0600

    added wee error checking to test case 01

--- run/01-pumping-well/run-cases.sh
+++ run/01-pumping-well/run-cases.sh
@@ -4,6 +4,12 @@ for t in  "contour" "hydrographs" "particle_track" ; do
     ./ltaem ./pumping_well_${t}.in \
         1> pumping_well_${t}.screen \
         2> pumping_well_${t}.err 
+    if [ -s pumping_well_${t}.err ] ; then
+        echo "**********************************"
+        echo "WARNING: ${t} produced an error"
+        echo "**********************************"
+        head pumping_well_${t}.err
+    fi
 done
 
 # specific PDEs associated with hydrograph types
@@ -12,4 +18,10 @@ for t in  "dualporosity" "leaky1" "leaky2" "leaky3" ; do
     ./ltaem ./pumping_well_${t}_hydrographs.in \
         1> pumping_well_${t}_hydrographs.screen \
         2> pumping_well_${t}_hydrographs.err
+    if [ -s pumping_well_${t}_hydrographs.err ] ; then
+        echo "**********************************"
+        echo "WARNING: ${t} produced an error"
+        echo "**********************************"
+        head pumping_well_${t}_hydrographs.err
+    fi
 done
