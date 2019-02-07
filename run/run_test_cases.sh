c072238b4982a2885db6cba05157d8ce84aa24a3
klkuhlm <klkuhlm@sandia.gov>
2019-02-07 08:52:17 -0700

    changes to makefile and run scripts for latest version of gfortran

--- run/run_test_cases.sh
+++ run/run_test_cases.sh
@@ -6,21 +6,22 @@ ln -sf ../ltaem .
 ./run-cases.sh
 cd ..
 
+
 for d in {01a-,02-,03-,04-,06-,07-,08-,09-,10-,11-}; do
     cd ${d}*
     echo "now in $(pwd)"
     ln -sf ../ltaem .
     for i in *.in; do
         echo "running ${i}"
-        ./ltaem ${i} > ${i}_screen.out
+        LD_LIBRARY_PATH=/usr/local/stow/gcc-trunk/lib64  ./ltaem ${i} > ${i}_screen.out
     done
     cd ..
 done
 
-cd 05-particle-sensitivity
-echo "now in $(pwd)"
-ln -sf ../ltaem .
-python particle-input.py > python_particle_input_screen.out
-python plot-results.py > python_plot_results_screen.out
-cd ..
+##cd 05-particle-sensitivity
+##echo "now in $(pwd)"
+##ln -sf ../ltaem .
+##python particle-input.py > python_particle_input_screen.out
+##python plot-results.py > python_plot_results_screen.out
+##cd ..
 
