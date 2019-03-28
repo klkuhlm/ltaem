15c5e08d0be24f03388e8320b395f25e6854596b
klkuhlm <klkuhlm@sandia.gov>
2019-03-28 10:55:42 -0600

    C arb wrapper compiles without warnings

--- fortran/bessel_arb_wrapper.c
+++ fortran/bessel_arb_wrapper.c
@@ -18,29 +18,29 @@ double _Complex arb_K(double gcc_nu, double _Complex gcc_z, int kode)
   acb_init(acb_z);
 
   // gcc_z -> acb_z (argument)
-  arf_set_round(acb_realref(acb_z), __real__(gcc_z), 53, ARF_RND_DOWN);
-  arf_set_round(acb_imagref(acb_z), __imag__(gcc_z), 53, ARF_RND_DOWN);
+  acb_set_d_d(acb_z, __real__(gcc_z), __imag__(gcc_z));
 
   // gcc_nu -> acb_nu (order)
-  arf_set_round(acb_realref(acb_nu), gcc_nu, 53, ARF_RND_DOWN);
+  acb_set_d(acb_nu, gcc_nu);
 
   int extra = 30;
   // compute Bessel function 
-  if (kode == 1) // unscaled
+  if (kode == 1){ // unscaled
     do {
       acb_hypgeom_bessel_k(acb_res, acb_nu, acb_z, 53 + extra);
       extra *= 2;
     } while (acb_rel_accuracy_bits(acb_res) < 53);
-  else // scaled
-    do {
-      acb_hypgeom_bessel_k_scaled(acb_res, acb_nu, acb_z, 53 + extra);
-      extra *= 2;
-    } while (acb_rel_accuracy_bits(acb_res) < 53);    
+  }
+  //else // scaled
+    //do {
+    //  acb_hypgeom_bessel_k_scaled(acb_res, acb_nu, acb_z, 53 + extra);
+    //  extra *= 2;
+    //} while (acb_rel_accuracy_bits(acb_res) < 53);    
       
   
   //  arb_res -> gcc_K
-  __real__(gcc_K) = arf_get_d(acb_realref(acb_res), ARF_RND_DOWN);
-  __imag__(gcc_K) = arf_get_d(acb_imagref(acb_res), ARF_RND_DOWN);
+  __real__(gcc_K) = arf_get_d(arb_midref(acb_realref(acb_res)), ARF_RND_DOWN);
+  __imag__(gcc_K) = arf_get_d(arb_midref(acb_imagref(acb_res)), ARF_RND_DOWN);
   
   acb_clear(acb_res);
   acb_clear(acb_nu);
@@ -65,29 +65,29 @@ double _Complex arb_I(double gcc_nu, double _Complex gcc_z, int kode)
   acb_init(acb_z);
 
   // gcc_z -> acb_z (argument)
-  arf_set_round(acb_realref(acb_z), __real__(gcc_z), 53, ARF_RND_DOWN);
-  arf_set_round(acb_imagref(acb_z), __imag__(gcc_z), 53, ARF_RND_DOWN);
+  acb_set_d_d(acb_z, __real__(gcc_z), __imag__(gcc_z));
 
   // gcc_nu -> acb_nu (order)
-  arf_set_round(acb_realref(acb_nu), gcc_nu, 53, ARF_RND_DOWN);
+  acb_set_d(acb_nu, gcc_nu);
 
   int extra = 30;
   // compute bessel function
-  if (kode == 1)
+  if (kode == 1){
     do {
       acb_hypgeom_bessel_i(acb_res, acb_nu, acb_z, 53 + extra);
       extra *= 2;
     } while (acb_rel_accuracy_bits(acb_res) < 53);
-  else
-    do {
-      acb_hypgeom_bessel_i_scaled(acb_res, acb_nu, acb_z, 53 + extra);
-      extra *= 2;
-    } while (acb_rel_accuracy_bits(acb_res) < 53);
+  }
+  //else
+    //do {
+    //  acb_hypgeom_bessel_i_scaled(acb_res, acb_nu, acb_z, 53 + extra);
+    //  extra *= 2;
+    //} while (acb_rel_accuracy_bits(acb_res) < 53);
     
   
   //  arb_res -> gcc_I
-  __real__(gcc_I) = arf_get_d(acb_realref(acb_res), ARF_RND_DOWN);
-  __imag__(gcc_I) = arf_get_d(acb_imagref(acb_res), ARF_RND_DOWN);
+  __real__(gcc_I) = arf_get_d(arb_midref(acb_realref(acb_res)), ARF_RND_DOWN);
+  __imag__(gcc_I) = arf_get_d(arb_midref(acb_imagref(acb_res)), ARF_RND_DOWN);
   
   acb_clear(acb_res);
   acb_clear(acb_nu);
