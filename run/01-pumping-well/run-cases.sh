f0bf1c192f25c6dfe435fa38ba3c7d653e815a7b
klkuhlm <kristopher.kuhlman@gmail.com>
2022-08-17 07:36:05 -0600

    updated path to dev compiler, found issue with test case 6

--- run/01-pumping-well/run-cases.sh
+++ run/01-pumping-well/run-cases.sh
@@ -2,7 +2,7 @@
 # "particle_track"  <<<< this was blowing up and producing gigabytes of output
 for t in  "contour" "hydrographs" ; do
     echo "pumping_well $t output"
-    LD_LIBRARY_PATH=/usr/local/stow/gcc-trunk/lib64 ./ltaem ./pumping_well_${t}.in \
+    LD_LIBRARY_PATH=/home/klkuhlm/src/gcc/local/lib64 ./ltaem ./pumping_well_${t}.in \
         1> pumping_well_${t}.screen \
         2> pumping_well_${t}.err 
     if [ -s pumping_well_${t}.err ] ; then
@@ -16,7 +16,7 @@ done
 # specific PDEs associated with hydrograph types
 for t in  "dualporosity" "leaky1" "leaky2" "leaky3" "stepwiseQ" "linearQ"; do 
     echo "pumping_well ${t} hydrographs"
-    LD_LIBRARY_PATH=/usr/local/stow/gcc-trunk/lib64 ./ltaem ./pumping_well_${t}_hydrographs.in \
+    LD_LIBRARY_PATH=/home/klkuhlm/src/gcc/local/lib64 ./ltaem ./pumping_well_${t}_hydrographs.in \
         1> pumping_well_${t}_hydrographs.screen \
         2> pumping_well_${t}_hydrographs.err
     if [ -s pumping_well_${t}_hydrographs.err ] ; then
